8f9984c13d36204d074a0fbddcb57ae7f51c6b88
Fabiano Monteiro <f.carvamonteiro@gmail.com>
2024-01-08 12:03:46 -0300

    Desafio concluído!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
-﻿using DesafioPOO.Models;
+﻿﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Nokia 1", imei: "11111111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone: ");
+Smartphone iphone = new Iphone(numero: "654321", modelo: "Nokia 2", imei: "22222222222", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
